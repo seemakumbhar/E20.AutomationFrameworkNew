a7060403f05eb58ff9bc6dbb532d10d51cd6a71a
seema <seema@192.168.1.40>
2025-05-20 17:47:48 +0530

    FrameworkCommitted

--- src/main/java/genericUtilities/BaseClass.java
+++ src/main/java/genericUtilities/BaseClass.java
@@ -1,6 +1,7 @@
 package genericUtilities;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
